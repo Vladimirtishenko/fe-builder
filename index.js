e081fe019e6c4e158622c17e4ed0f15306c5afea
vladimirtishenko <vladimirtishenko1@gmail.com>
2017-05-27 11:41:02 +0300

    New version (Bug fix)

--- index.js
+++ index.js
@@ -1,88 +1,80 @@
 var fs = require('fs');
 var source = require('./stucture.json');
 var package = require('./package.json').dependencies;
-var glob = require('glob');
 
-require('child_process').exec('npm init --yes', function (error, out) {
-	var pathToPackage =  __dirname + '/package.json';
-	if(!error){
-		fs.readFile(pathToPackage, 'utf8', function(err, contents) {
-			let json = JSON.parse(contents);
-		    json.dependencies = package;
-		    fs.writeFile (pathToPackage, JSON.stringify(json), function(err) {
-                if (err) console.log('File package.json not created!');
-                console.log('package.json created');
-            });
-		});
-	}
-});
-
-module.exports = (function () {
-
-new glob.Glob('./source/.npmignore')
-	.on('match', function(path) {
-		fs.rename(path, path.replace(/\.npmignore$/, '.gitignore'), function(err) {
-			if (err) throw err;
-		});
-	})
-	.on('error', function(err) {
-		throw err;
-});
+module.exports = (function() {
 
 
-(function createTree(tree, folder){
+    (function basicInit() {
 
-for(var mark in tree){
+        require('child_process').exec('npm init --yes', function(error, out) {
+            var pathToPackage = __dirname + '/package.json',
+                gitignore = __dirname + '/source/.gitignore',
+                npmignore = __dirname + '/source/.npmignore',
+                read,
+                json;
 
-	if(typeof tree[mark] == 'object'){
-		if (!fs.existsSync(folder + mark)){
-		    fs.mkdirSync(folder + mark);
-		}
-		folder += mark + "/";
-		createTree(tree[mark], folder);
-		folder = "public/";
+            if (!error) {
+            	try {
+            		read = fs.readFileSync("package.json", 'utf8');
+	                json = JSON.parse(read);
+	                json.dependencies = package;
+	                fs.writeFileSync("package.json", JSON.stringify(json, null, 4))
+	                fs.renameSync(__dirname + '/source/.npmignore', __dirname + '/source/.gitignore')
+            	} catch(e){
+            		console.log(e);
+            	}
+            }
+            createTree(source, "")
+        });
 
-	} else {
-		if(tree[mark] == ""){
-			if (!fs.existsSync(folder + mark)){
-			    fs.mkdirSync(folder + mark);
-			}
-		} 
-		else if(!isNaN(mark)){
-			fs.createReadStream(__dirname + '/source/'+tree[mark]).pipe(fs.createWriteStream(folder+tree[mark]));
-		}
-		else if(mark == "$"){
-			cycleCopy(tree[mark], "")
-		}
-		else {
-			if (!fs.existsSync(folder + mark)){
-			    fs.mkdirSync(folder + mark);
+    }())
 
-			    cycleCopy(tree[mark], folder + mark+"/")  
+    function createTree(tree, folder) {
 
-			}
-		}
-	}
+        for (var mark in tree) {
 
-}
+            if (typeof tree[mark] == 'object') {
+                if (!fs.existsSync(folder + mark)) {
+                    fs.mkdirSync(folder + mark);
+                }
+                folder += mark + "/";
+                createTree(tree[mark], folder);
+                folder = "public/";
 
-})(source, "")
+            } else {
+                if (tree[mark] == "") {
+                    if (!fs.existsSync(folder + mark)) {
+                        fs.mkdirSync(folder + mark);
+                    }
+                } else if (!isNaN(mark)) {
+                    fs.createReadStream(__dirname + '/source/' + tree[mark]).pipe(fs.createWriteStream(folder + tree[mark]));
+                } else if (mark == "$") {
+                    cycleCopy(tree[mark], "")
+                } else {
+                    if (!fs.existsSync(folder + mark)) {
+                        fs.mkdirSync(folder + mark);
 
+                        cycleCopy(tree[mark], folder + mark + "/")
 
-function cycleCopy(tree, folder) {
-	var listFiles = tree.split(', ');
-    for (var i = listFiles.length - 1; i >= 0; i--) {
+                    }
+                }
+            }
 
-    	if(listFiles[i].indexOf('http') > -1){
-    		require('child_process').exec('git clone https://github.com/Vladimirtishenko/core-ui-framework.git ' + folder);
-    	} else {
-    		fs.createReadStream(__dirname +  '/source/'+listFiles[i]).pipe(fs.createWriteStream(folder+listFiles[i]));	
-    	}	
+        }
+
     }
-}
+
+    function cycleCopy(tree, folder) {
+        var listFiles = tree.split(', ');
+        for (var i = listFiles.length - 1; i >= 0; i--) {
+
+            if (listFiles[i].indexOf('http') > -1) {
+                require('child_process').exec('git clone https://github.com/Vladimirtishenko/core-ui-framework.git ' + folder);
+            } else {
+                fs.createReadStream(__dirname + '/source/' + listFiles[i]).pipe(fs.createWriteStream(folder + listFiles[i]));
+            }
+        }
+    }
 
 })();
-
-
-
-
