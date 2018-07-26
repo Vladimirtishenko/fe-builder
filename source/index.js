938818d24e368eeb24aaf60d06ab382f65a81283
Владимир Тищенко <valdimirtishenko>
2018-07-26 20:03:53 +0300

    New version with webpack 4

---
+++ source/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import '../stylus/build.styl';
+
+import 'file!pug!../pug/index.pug';
+
+import './app.js'
--- source/webpack.config.js
+++ source/webpack.config.js
@@ -2,76 +2,77 @@
 
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const stylusLoader = ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader?minimize!stylus-loader' });
 const autoprefixer = require('autoprefixer');
-const NODE_ENV = process.env.NODE_ENV || "development";
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const environment = process.env.NODE_ENV || 'development';
 
-let config = [{
+const config = {
     name: 'js',
     entry: {
-        app: './public/js/app.js',
-        core: './public/js/core/core.js'
+        app: './public/js/index.js'
     },
     output: {
-        path: __dirname  + "/public/build/",
+        path: __dirname  + "/public/build",
         filename: 'build.[name].js',
-        publicPath: './public/build/'
+        publicPath: './build'
     },
     module: {
-        loaders: [
+        rules: [
             {
+                use: 'babel-loader',
                 test: /\.js?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    resolve: {
-        modules: ["node_modules"],
-        extensions: [".js", "css", "styl", "woff", "ttf", "otf", "jpg", "png", "gif"]
-    },
-    plugins: [
-        new UglifyJSPlugin()
-    ]
-}, {
-    name: 'styles',
-    entry: {
-        styles: "./public/styl/build.styl",
-    },
-    output: {
-        path: __dirname + '/public/build/',
-        filename: '[name].min.css'
-    },
-    module: {
-      rules: [
-            {
-               test: /\.styl$/, 
-               loader: ExtractTextPlugin.extract({ fallback: 'style-loader', 
-                    use: [
-                    'css-loader?minimize!',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: function () {
-                                return [autoprefixer()]
-                            }
-                        }
-                    },
-                    'stylus-loader'
-                    ]
-                }) 
+                exclude: /node_modules/
             },
             {
-                test: /\.(png|woff|woff2|eot|ttf|otf|svg|jpg|jpeg|gif)$/, 
-                loader: 'url-loader?limit=100000'
-            }
-        ]  
+                use: [ 'style-loader', 'css-loader' ],
+                test: /\.css?$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.pug?$/,
+                use: [
+                  "file-loader?name=../../[name].html",
+                  "extract-loader",
+                  "html-loader",
+                  "pug-html-loader"
+                ],
+                exclude: /node_modules/
+            },
+            {
+                 test: /\.styl$/, 
+                 loader: ExtractTextPlugin.extract({ fallback: 'style-loader', 
+                      use: [
+                      'css-loader?minimize!',
+                      {
+                          loader: 'postcss-loader',
+                          options: {
+                              plugins: function () {
+                                  return [autoprefixer()]
+                              },
+                              sourceMap: 'inline'
+                          }
+                      },
+                      'stylus-loader'
+                      ]
+                  }) 
+              },
+              {
+                  test: /\.(png|woff|woff2|otf|eot|ttf|svg|jpg|jpeg)$/, 
+                  loader: 'file-loader?limit=100000'
+              }
+        ]
     },
+    performance: { hints: false },
+    optimization: {},
+    mode: environment,
     plugins: [
-        new ExtractTextPlugin("[name].min.css"),
-    ]    
-}];
+        new ExtractTextPlugin("build.[name].css")    ]
+};
+
+if (environment === 'production') {
+    config.optimization.minimize = true;
+    babelSettings.plugins.push("transform-react-inline-elements");
+    babelSettings.plugins.push("transform-react-constant-elements");
+}
 
 
 module.exports = config;
